5ff0a2d3272de59d749497fe344bf7ffb5587e9e
ChrissDisigale <chrispro.genius@gmail.com>
2024-06-13 04:26:28 +0300

    empty screen

--- lib/main.dart
+++ lib/main.dart
@@ -46,39 +46,41 @@ class _MyHomePageState extends State<MyHomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Color(0xFFB45CFF),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Container(
-              height: 230,
-              padding: EdgeInsets.all(32),
-              child: Image.asset('images/emptyScreen1/allTaskDone2.png'),
-              decoration: BoxDecoration(color: Color(0xFFAE56FA), shape: BoxShape.circle),
-            ),
-            48.height,
-            Text('All Task Done!', style: boldTextStyle(size: 20, color: white)),
-            16.height,
-            Text(
-              "cDisigale",
-              style: primaryTextStyle(size: 15, color: white),
-              textAlign: TextAlign.center,
-            ).paddingSymmetric(vertical: 8, horizontal: 60),
-            50.height,
-            AppButton(
-              shapeBorder: RoundedRectangleBorder(borderRadius: radius(30)),
-              elevation: 10,
-              onTap: () {
-                toast('Review');
-              },
-              child: Text(
-                'Review',
-                style: boldTextStyle(
-                  color: Color(0xFFB45CFF),
-                ),
-              ).paddingSymmetric(horizontal: 32),
-            ),
-            100.height,
-          ],
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 230,
+                padding: EdgeInsets.all(32),
+                child: Image.asset('images/emptyScreen1/allTaskDone2.png'),
+                decoration: BoxDecoration(color: Color(0xFFAE56FA), shape: BoxShape.circle),
+              ),
+              48.height,
+              Text('All Task Done!', style: boldTextStyle(size: 20, color: white)),
+              16.height,
+              Text(
+                "cDisigale",
+                style: primaryTextStyle(size: 15, color: white),
+                textAlign: TextAlign.center,
+              ).paddingSymmetric(vertical: 8, horizontal: 60),
+              50.height,
+              AppButton(
+                shapeBorder: RoundedRectangleBorder(borderRadius: radius(30)),
+                elevation: 10,
+                onTap: () {
+                  toast('Review');
+                },
+                child: Text(
+                  'Review',
+                  style: boldTextStyle(
+                    color: Color(0xFFB45CFF),
+                  ),
+                ).paddingSymmetric(horizontal: 32),
+              ),
+              100.height,
+            ],
+          ),
         ),
       ),
     );
